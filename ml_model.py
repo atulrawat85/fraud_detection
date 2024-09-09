94ef4aa478c34f11cb23a6640f9284416d0c27df
atulrawat85 <atul.rawat85@gmail.com>
2024-09-09 18:00:48 +0530

    added 3 ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("adding all ml related code")
+
+
+
+def model1():
+	print("adding code for ml model1")
+	
+def model2():
+	print("adding code for ml model2")
+
+
+def model3():
+	print("adding code for ml model3")
